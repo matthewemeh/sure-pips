188b82844f4eacf289973cdc96a670f6e091e09b
Matthew Emeh <mcmekus10@gmail.com>
2022-05-07 03:00:11 +0100

    changes to error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Error.css';
 
 const Error = () => {
-  return <h1>ERROR! PAGE NOT FOUND</h1>;
+  return <h1 className='error'>ERROR! PAGE NOT FOUND</h1>;
 };
 
 export default Error;
